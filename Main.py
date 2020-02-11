0f17cdaf93c01eeb09bff30666032bcbee13258b
noobpersonTOs <46160387+noobpersonTOs@users.noreply.github.com>
2020-02-10 23:53:36 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,15 @@
 import discord
 from discord.ext import commands
+import os
 
 @bot.event
 async def on_ready():
+  print("Logged in as")
+  print(bot.user.name)
+  print(bot.user.id)
+  
+@bot.command()
+async def wave(ctx):
+  await ctx.send("Hi 👋")
+  
+bot.run(os
